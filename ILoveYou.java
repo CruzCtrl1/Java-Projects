4b8381dc2dd5a83e15c89838315dba41164f2f50
CruzCtrl1 <41966126+CruzCtrl1@users.noreply.github.com>
2018-07-31 10:42:08 -0700

    Update ILoveYou.java

--- ILoveYou.java
+++ ILoveYou.java
@@ -1,15 +1,11 @@
 import javax.swing.JOptionPane;
-//import java.util.Scanner;
 class ILoveYou{
     public static void main(String[] args){
         String name;
         //Scanner name = new Scanner();
         name = JOptionPane.showInputDialog("Enter your name:");
-        //System.out.println(name);
-        String bae = "Wendy";
-        //System.out.println(bae);
+        String bae = "bae";
         Boolean check = name.equals(bae);
-        //System.out.println(check);
         if(check == true){
             JOptionPane.showMessageDialog(null, "I love you " + name + "! You are so beautiful.");
         }
@@ -19,7 +15,3 @@ class ILoveYou{
         System.exit(0);
     }
 }
-
-//Finally got it working! Can't set two strings equal to each other with ==, as it checks the addresses (not the same)
-//Instead, I have to use the object.equals(object)
-//Can also do this with a scanner.
