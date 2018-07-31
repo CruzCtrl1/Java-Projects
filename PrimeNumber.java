aaad2c4f724cdbcb16a891f018cb8f634d156143
CruzCtrl1 <41966126+CruzCtrl1@users.noreply.github.com>
2018-07-31 10:48:04 -0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -28,32 +28,3 @@ public class PrimeNumber{
         if (isPrime == true){
             System.out.println("Your number is a prime number");
         }
-
-        //Program works
-        
-            /*
-
-            if(result==remainder){
-                System.out.println("Your input is divisible by " + start + " and proves your input is not a prime number");
-            }else{
-                System.out.println("Your input is divisible by " + start + " proves your input is a prime number");
-            }
-            
-            */
-
-    }
-}
-
-/*
-Pseudo Code:
-A prime number is a number that can only be divided by 1 and by itself. 
-To determine this, I should use an for loop loop. Start at an index = 2. (if
-I start at 1, it will be true)
-Go up in increments of one until I get to the specified number.
-If it is a prime number, then only 1 and itself should divide into the number
-*/
-
-// If the remainder = 0, then the dividend divides precisely into the input.
-
-//If the remainder=0 when input/input, then it is a prime number. Anything
-// in between, then it is not a prime number
